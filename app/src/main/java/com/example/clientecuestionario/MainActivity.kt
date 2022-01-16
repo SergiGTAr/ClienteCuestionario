cac81d0b4defd355f61d0a452f3ecebe64f15a42
Sergi García Tárraga <sergi.garcia@uvic.cat>
2022-01-16 23:34:47 +0100

    Getvalue of db

--- app/src/main/java/com/example/clientecuestionario/MainActivity.kt
+++ app/src/main/java/com/example/clientecuestionario/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.clientecuestionario
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun btnEnviar(view: View) {
+        val dbinstance = FirebaseDatabase.getInstance("https://servidorquestionari-default-rtdb.europe-west1.firebasedatabase.app")
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("partida")
+        val id = myRef.child("id").getValue()
     }
 }
